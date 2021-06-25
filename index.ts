01ff5fcd42c2fffd6f6ce5d263812c7613e93be6
rpeddakama <rishipeddakama@gmail.com>
2021-06-25 16:36:13 -0700

    css?

--- index.ts
+++ index.ts
@@ -10,6 +10,7 @@ app.use(express.json() as RequestHandler)
 app.use(express.urlencoded({ extended: true }) as RequestHandler)
 app.use(cors())
 app.set("view engine", "ejs")
+app.use(express.static(__dirname + "public"))
 
 app.get("/", async (req, res) => {
   let files = await fs.readdir("./problems")
